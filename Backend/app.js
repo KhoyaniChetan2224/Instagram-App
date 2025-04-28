e5b3ec0c0e5bdac6dde0555b936999020e26efe5
Khoyani Chetan <erchetankhoyani@gmail.com>
2025-04-28 10:44:54 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,25 +1,25 @@
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
 dotenv.config();
-const express = require('express');
+import express from 'express';
+import morgan from 'morgan';
+import userRoutes from './routes/user.routes.js';
+import aiRoutes from './routes/ai.routes.js';
+import postsRoutes from "./routes/posts.routes.js"
+import cookieParser from 'cookie-parser';
+
+
+
 const app = express();
-const cors = require('cors');
-const connectToDB = require('./db/db');
-const userRoutes = require('./routes/user.routes');
-
-connectToDB();
-
-
-app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use('/users', userRoutes);
+app.use('/ai', aiRoutes);
+app.use('/posts', postsRoutes);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello Instagram - Application in MERN Stack Project');
-});
-
+export default app;
 app.use('/users', userRoutes); // User routes
 
 module.exports = app; // Start the server
