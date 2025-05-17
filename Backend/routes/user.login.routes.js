2ab47f83d7f2eeab17ebdfa50bc9b70959561f35
Khoyani Chetan <erchetankhoyani@gmail.com>
2025-05-17 15:30:29 +0530

    Update user.login.routes.js

--- Backend/routes/user.login.routes.js
+++ Backend/routes/user.login.routes.js
@@ -2,16 +2,39 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const userController = require('../controllers/user.login.controllers');
-
+const profileControoler = require('../controllers/user.profile.controllers');
 
 router.post('/UserSignup', [
-    body('mobilnumber').isMobilePhone().withMessage('Mobil number must be at least 10 character long'),
-    body('fullname').isLength({min: 3}).withMessage('Full name must be at least 3 character long'),
-    body('password').isLength().withMessage('password must be at least 6 character long'),
-    body('email').isEmail().withMessage('Invali Email')
-], 
-    userController.registerUser
-)
+    body('email').isEmail().withMessage('Please include a valid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    body('fullname').notEmpty().withMessage('Full Name is required'),
+    body('username').notEmpty().withMessage('Username is required'),
+], userController.registerUser);
+
+router.post('/login', [
+    body('username').notEmpty().withMessage('Username is required'),
+    body('password').notEmpty().withMessage('Password is required'),
+], userController.loginUser);
+
+router.post('/emailsingup', [
+    body('month').notEmpty().withMessage('Month is required'),
+    body('year').notEmpty().withMessage('Year is required'),
+    body('day').notEmpty().withMessage('Day is required'),
+], userController.birthDataVarification);
+
+router.post('/robotsecurity', [
+    body('chackbox').notEmpty().withMessage('Checkbox is required'),
+], userController.robotSecurity);
+
+
+router.get('/profile', [
+    body('userId').notEmpty().withMessage('User ID is required'),
+    body('username').notEmpty().withMessage('Username is required'),
+    body('bio').optional().isString(),
+    body('profilePicture').optional().isURL().withMessage('Profile picture must be a valid URL'),
+    body('website').optional().isURL().withMessage('Website must be a valid URL'),
+], profileControoler.createUserProfile);
+
 
 
 module.exports = router;
